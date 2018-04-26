555e47e98dd3412487f0e4e96ad32eddcf2f8779
lynkas <cat@catting.net>
2018-04-26 12:07:26 +0800

    add blur and mask to header

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?php bloginfo('name')?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="./ystyle.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="http://10.7.26.36/wordpress/wp-content/themes/camo/style.css" />
     <script src="main.js"></script>
+    <?php wp_head(); ?>
 </head>
 <body>
     <?php get_header();?>
